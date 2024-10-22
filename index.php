7da64c3543e029888ddbb54b7330528aeccc869d
shishumandirhalvad <shishumandirhalvadwebsite@gmail.com>
2024-10-22 11:34:43 -0400

    Add files via upload

---
+++ index.php
@@ -0,0 +1,348 @@
+<?php include 'header.php';?>
+
+<!-- about -->
+	<div class="about-w3layouts">
+		<div class="container">
+			<div class="col-md-6 col-sm-6 gallery-grids agileits w3layouts gallery-grids1 wow slideInLeft">
+				<div class="gallery-grid-images agileits w3layouts">
+					<div class="col-md-4 col-sm-4 gallery-grid gallery-grid-1 history-grid-image">
+						<img src="images/about/about1.jpg" alt="Agileits W3layouts" class="zoom-img">
+					</div>
+					<div class="col-md-4 col-sm-4 gallery-grid gallery-grid-2 history-grid-image">
+						<img src="images/about/about2.jpg" alt="Agileits W3layouts" class="zoom-img">
+					</div>
+					<div class="col-md-4 col-sm-4 gallery-grid gallery-grid-3 history-grid-image">
+						<img src="images/about/about3.jpg" alt="Agileits W3layouts" class="zoom-img">
+					</div>
+					<div class="col-md-4 col-sm-4 gallery-grid gallery-grid-4 history-grid-image">
+						<img src="images/about/about4.jpg" alt="Agileits W3layouts" class="zoom-img">
+					</div>
+					<div class="col-md-4 col-sm-4 gallery-grid gallery-grid-5 history-grid-image">
+						<img src="images/about/about5.jpg" alt="Agileits W3layouts" class="zoom-img">
+					</div>
+					<div class="col-md-4 col-sm-4 gallery-grid gallery-grid-6 history-grid-image">
+						<img src="images/about/about6.jpg" alt="Agileits W3layouts" class="zoom-img">
+					</div>
+					<div class="clearfix"></div>
+				</div>
+			</div>
+
+			<div class="col-md-6 col-sm-6 gallery-grids agileits w3layouts gallery-grids2 wow slideInRight">
+				<h2 class="tittle-agileits-w3layouts">શાળા વિશે</h2>
+				
+				<p class="para-w3-agile">વિદ્યાભારતી સંલગ્ન રાષ્ટ્રીય શિક્ષણ સેવા પ્રતિષ્ઠાન સંચાલિત શ્રી સરસ્વતી શિશુમંદિર હળવદ
+				</p>
+				
+				<a href="single.php" class="button-w3layouts hvr-rectangle-out">વધુ મહિતી</a>
+			</div>
+			<div class="clearfix"></div>
+			
+		</div>
+	</div>
+	<!-- //about -->
+
+<!--/services-->
+<div class="services-w3-agileits w3agileits-ref">
+	<div class="col-md-6 services-left">
+		<div class="service-grid1">
+			<div class="col-md-4 service-grids-w3ls">
+				<!-- <i class="fa fa-language" aria-hidden="true"></i> -->
+				<h5>શિક્ષણના વિવિધક્ષેત્રે કાર્યો કરવા , શાળાઓ ચલાવી</h5>
+				
+			</div>
+			<div class="col-md-4 service-grids-w3ls">
+<!-- 				<i class="fa fa-users" aria-hidden="true"></i> -->
+				<h5>સરસ્વતી માં.ઉ.માં.વિદ્યામંદિર</h5>
+			</div>
+			<div class="col-md-4 service-grids-w3ls">
+<!-- 				<i class="fa fa-graduation-cap" aria-hidden="true"></i>
+ -->				<h5>શ્રી સરસ્વતી સંતરામ શિશુવાટિકા </h5>
+			</div>
+			<div class="clearfix"></div>
+		</div>
+		<div class="service-grid2">
+			<div class="col-md-4 service-grids-w3ls">
+<!-- 				<i class="fa fa-cutlery" aria-hidden="true"></i> -->
+				<h5>એમ.એચ.રાવલ.કુમાર/કન્યા છાત્રાલય</h5>
+			</div>
+			<div class="col-md-4 service-grids-w3ls">
+<!-- 				<i class="fa fa-bus" aria-hidden="true"></i> -->
+				<h5>સમન્વય ધ્યાન યોગ કેન્દ્ર</h5>
+
+			</div>
+			<div class="col-md-4 service-grids-w3ls">
+<!-- 				<i class="fa fa-calendar-check-o" aria-hidden="true"></i> -->
+				<h5>ઈપ્કોવાળા લાઈબ્રેરી</h5>
+			</div>
+			<div class="clearfix"></div>
+		</div>		
+	</div>
+	<div class="col-md-6  services-right">
+		<div class="services-info">
+			<h3 class="tittle-agileits-w3layouts white-w3ls">સંસ્થા દ્વારા ચાલતા સેવાકીય એકમો(પ્રકલ્પો)</h3>
+			<p class="para-w3-agile white-w3ls"></p>
+			<a href="single.php" class="button-w3layouts hvr-rectangle-out">વધુ મહિતી</a>
+		</div>
+	</div>
+	<div class="clearfix"></div>
+</div>
+<!--//services-->
+<!-- agile_testimonials -->
+<!-- <div class="test">
+	<div class="container">
+	<div class="col-md-3 test-left-agileinfo">
+	<h3 class="tittle-agileits-w3layouts">Testimonials</h3>
+	</div>
+		<div class="col-md-9 test-gr">
+					<div class=" test-gri1">
+					 <div id="owl-demo2" class="owl-carousel">
+							<div class="agile">
+							   	<div class="test-grid">
+							   		<div class="col-md-8 test-grid1">
+										<p class="para-w3-agile">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis.
+										Lorem ipsum dolor .</p>
+										<h4>Steve Warner</h4>
+										<span>Lorem Ipsum</span>
+									</div>	
+									<div class="col-md-4 test-grid2">
+										<img src="images/t1.jpg" alt="" class="img-r">
+									</div>
+								</div>	
+								<div class="clearfix"></div>
+							</div>
+							<div class="agile">
+							   	<div class="test-grid">
+							   		<div class="col-md-8 test-grid1">
+										<p class="para-w3-agile">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis.
+										Lorem ipsum dolor.</p>
+										<h4>Andery</h4>
+										<span>Lorem Ipsum</span>
+									</div>	
+									<div class="col-md-4 test-grid2">
+										<img src="images/t2.jpg" alt="" class="img-r">
+									</div>
+								</div>	
+								<div class="clearfix"></div>
+							</div>
+							<div class="agile">
+							   	<div class="test-grid">
+							   		<div class="col-md-8 test-grid1">
+										<p class="para-w3-agile">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis.
+										Lorem ipsum dolor .</p>
+										<h4>williams</h4>
+										<span>Lorem Ipsum</span>
+									</div>	
+									<div class="col-md-4 test-grid2">
+										<img src="images/t3.jpg" alt="" class="img-r">
+									</div>
+								</div>	
+								<div class="clearfix"></div>
+							</div>
+							<div class="agile">
+							   	<div class="test-grid">
+							   		<div class="col-md-8 test-grid1">
+										<p class="para-w3-agile">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis.
+										Lorem ipsum dolor .</p>
+										<h4>Shane Smith</h4>
+										<span>Lorem Ipsum</span>
+									</div>	
+									<div class="col-md-4 test-grid2">
+										<img src="images/t4.jpg" alt="" class="img-r">
+									</div>
+								</div>	
+								<div class="clearfix"></div>
+							</div>	
+					</div>
+				</div>	
+		</div>
+	</div>
+</div> -->
+<!-- //agile_testimonials -->
+<!-- computer lab videos -->
+<div class="register-wthree">
+	<div class="container">
+	<div class="col-md-12 regstr-l-w3-agileits">
+	<h3 class="tittle-agileits-w3layouts white-w3ls">કમ્પ્યુટર લેબ</span></h3>
+	
+				<div class="clearfix"></div>
+	
+	</div>
+	<div class="col-md-4 regstr-r-w3-agileits">
+		<video width="320" height="240" controls>
+  			<source src="video/BMS_9294.mp4" type="video/mp4">
+			Your browser does not support the video tag.
+		</video>	
+	</div>
+	<div class="col-md-4 regstr-r-w3-agileits">
+		<video width="320" height="240" controls>
+  			<source src="video/BMS_9295.mp4" type="video/mp4">
+			Your browser does not support the video tag.
+		</video>	
+	</div>
+	<div class="col-md-4 regstr-r-w3-agileits">
+		<video width="320" height="240" controls>
+  			<source src="video/BMS_9296.mp4" type="video/mp4">
+			Your browser does not support the video tag.
+		</video>	
+	</div>
+	<div class="col-md-4 regstr-r-w3-agileits">
+		<video width="320" height="240" controls>
+  			<source src="video/BMS_9297.mp4" type="video/mp4">
+			Your browser does not support the video tag.
+		</video>	
+	</div>
+	<div class="col-md-4 regstr-r-w3-agileits">
+		<video width="320" height="240" controls>
+  			<source src="video/BMS_9298.mp4" type="video/mp4">
+			Your browser does not support the video tag.
+		</video>	
+	</div>
+	<div class="col-md-4 regstr-r-w3-agileits">
+		<video width="320" height="240" controls>
+  			<source src="video/BMS_9299.mp4" type="video/mp4">
+			Your browser does not support the video tag.
+		</video>	
+	</div>
+	<div class="clearfix"></div>
+	</div>
+</div>
+<!-- //computer lab videos -->
+
+<!-- hosttel rooms -->
+	<div class="about-w3layouts">
+		<div class="container">
+
+			<div class="col-md-12 col-sm-12 gallery-grids agileits w3layouts gallery-grids2 wow slideInRight">
+				<h2 class="tittle-agileits-w3layouts">હોસ્ટેલ સુવિધા</h2>
+			
+			</div>
+
+			<div class="welcome">
+		<div class="container">
+			<div class="w3layouts_gallery_grids">	
+
+				<div class="col-md-4 w3layouts_gallery_grid">
+					<a href="images/photos/hostel/img1.jpg" class="lsb-preview" data-lsb-group="header">
+						<div class="w3layouts_news_grid">
+							<img src="images/photos/hostel/img1.jpg" alt=" " class="img-responsive">
+							<div class="w3layouts_news_grid_pos">
+							</div>
+						</div>
+					</a>
+				</div>
+				<div class="col-md-4 w3layouts_gallery_grid">
+					<a href="images/photos/hostel/img2.jpg" class="lsb-preview" data-lsb-group="header">
+						<div class="w3layouts_news_grid">
+							<img src="images/photos/hostel/img2.jpg" alt=" " class="img-responsive">
+							<div class="w3layouts_news_grid_pos">
+							</div>
+						</div>
+					</a>
+				</div>
+				<div class="col-md-4 w3layouts_gallery_grid">
+					<a href="images/photos/hostel/img3.jpg" class="lsb-preview" data-lsb-group="header">
+						<div class="w3layouts_news_grid">
+							<img src="images/photos/hostel/img3.jpg" alt=" " class="img-responsive">
+							<div class="w3layouts_news_grid_pos">
+							</div>
+						</div>
+					</a>
+				</div>
+				<div class="col-md-4 w3layouts_gallery_grid">
+					<a href="images/photos/hostel/img4.jpg" class="lsb-preview" data-lsb-group="header">
+						<div class="w3layouts_news_grid">
+							<img src="images/photos/hostel/img4.jpg" alt=" " class="img-responsive">
+							<div class="w3layouts_news_grid_pos">
+							</div>
+						</div>
+					</a>
+				</div>
+				<div class="col-md-4 w3layouts_gallery_grid">
+					<a href="images/photos/hostel/img5.jpg" class="lsb-preview" data-lsb-group="header">
+						<div class="w3layouts_news_grid">
+							<img src="images/photos/hostel/img5.jpg" alt=" " class="img-responsive">
+							<div class="w3layouts_news_grid_pos">
+							</div>
+						</div>
+					</a>
+				</div>
+				<div class="col-md-4 w3layouts_gallery_grid">
+					<a href="images/photos/hostel/img6.jpg" class="lsb-preview" data-lsb-group="header">
+						<div class="w3layouts_news_grid">
+							<img src="images/photos/hostel/img6.jpg" alt=" " class="img-responsive">
+							<div class="w3layouts_news_grid_pos">
+							</div>
+						</div>
+					</a>
+				</div>
+				<div class="col-md-4 w3layouts_gallery_grid">
+					<a href="images/photos/hostel/img7.jpg" class="lsb-preview" data-lsb-group="header">
+						<div class="w3layouts_news_grid">
+							<img src="images/photos/hostel/img7.jpg" alt=" " class="img-responsive">
+							<div class="w3layouts_news_grid_pos">
+							</div>
+						</div>
+					</a>
+				</div>
+			</div>
+		</div>
+	</div>
+
+
+			<div class="clearfix"></div>
+			
+		</div>
+	</div>
+	<!-- //hostel rooms -->
+
+
+<!-- hospital details -->
+<div class="register-wthree">
+	<div class="container">
+	<div class="col-md-12 regstr-l-w3-agileits">
+	<h3 class="tittle-agileits-w3layouts white-w3ls">જીવન જ્યોત જનરલ હોસ્પિટલ</span></h3>
+	
+				<div class="clearfix"></div>
+	
+	</div>
+	<div class="col-md-4 regstr-r-w3-agileits">
+		<video width="320" height="240" controls>
+  			<source src="video/hospital/hospital_opening.mp4" type="video/mp4">
+			Your browser does not support the video tag.
+		</video>	
+	</div>
+	<div class="col-md-4 regstr-r-w3-agileits">
+		<video width="320" height="240" controls>
+  			<source src="video/hospital/158660398_476420830067361_1076702490029744448_n.mp4" type="video/mp4">
+			Your browser does not support the video tag.
+		</video>	
+	</div>
+	<div class="col-md-4 regstr-r-w3-agileits">
+		<video width="320" height="240" controls>
+  			<source src="video/hospital/10000000_1081885295631141_8128017542561688967_n.mp4" type="video/mp4">
+			Your browser does not support the video tag.
+		</video>	
+	</div>
+	
+	<div class="clearfix"></div>
+	</div>
+</div>
+<!-- //hospital rooms -->
+
+<?php include("footer.php"); ?>
+<?php include("copyright.php"); ?>
+
+<?php include("uitotop.php"); ?>
+<script src="js/lsb.min.js"></script>
+	<script>
+	$(window).load(function() {
+		  $.fn.lightspeedBox();
+		});
+
+	$('audio,video').bind('play', function() {
+  activated = this;
+  $('audio,video').each(function() {
+    if(this != activated) this.pause();
+  });
+});
+	</script>
